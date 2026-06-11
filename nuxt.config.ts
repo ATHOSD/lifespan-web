989a9772d59fe32f35621b9d939059db948b46be
Dingyi Zhang <zhangdy1013@gmail.com>
2026-06-11 12:18:05 -0400

    fix: disable SSR to avoid NiiVue build issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 export default defineNuxtConfig({
+  ssr: false,
   runtimeConfig: {
     replicateApiToken: process.env.REPLICATE_API_TOKEN,
     replicateVersion: process.env.REPLICATE_VERSION || 'fb0ec1932828ca3348a57f38ec7086ac128e76413726ac15534424474b807934',
